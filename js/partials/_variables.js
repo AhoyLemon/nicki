6dc857fc8e36b59828f61d73418f33db682b863d
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2024-01-10 09:05:05 -0600

    Adjustments to game over screen and volume.

--- js/partials/_variables.js
+++ js/partials/_variables.js
@@ -2,13 +2,16 @@ const siteURL = "";
 
 const sounds = {
   yes: new Howl({
-    src: ["audio/achievement-message-tone.mp3"]
+    src: ["audio/achievement-message-tone.mp3"],
+    volume: 0.5
   }),
   no: new Howl({
-    src: ["audio/decay-475.mp3", "audio/decay-475.ogg"]
+    src: ["audio/decay-475.mp3", "audio/decay-475.ogg"],
+    volume: 0.5
   }),
   gameOver: new Howl({
-    src: ["audio/sour-tennessee-red.mp3", "audio/sour-tennessee-red.ogg"]
+    src: ["audio/sour-tennessee-red.mp3", "audio/sour-tennessee-red.ogg"],
+    volume:0.5
   })
 
 }
